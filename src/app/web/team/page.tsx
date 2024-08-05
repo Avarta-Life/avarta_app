65c135eeff86eac5e31de6a722962decdaa454d6
Surya Charan Teja Pandiripalli <71783906+suryacharanteja@users.noreply.github.com>
2024-08-05 19:05:01 +0530

    Update page.tsx adding the designation2
    Update page.tsx adding the designation2

--- src/app/web/team/page.tsx
+++ src/app/web/team/page.tsx
@@ -96,7 +96,7 @@ export default function Team(props: ITeamProps) {
 const team = [
   {
     title: "PN Chandra Ramu",
-    desig: "Founder, CEO & Principal GenAI Scientist",
+    desig: "Founder, CEO & GenAI Head",
     email: "savinaykumar@outlook.com",
     linkedin: "http://www.linkedin.com/in/chandraai/",
     image: "/assets/team/chandra.png",
