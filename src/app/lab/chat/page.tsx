bcc6086644079d245b2faa64d5ebfe16819a8ae2
Savinay Kumar <savinay@zeliot.in>
2024-08-07 07:18:08 +0530

    renormalize

--- src/app/lab/chat/page.tsx
+++
@@ -1,5 +0,0 @@
-export interface IChatPageProps {}
-
-export default function ChatPage(props: IChatPageProps) {
-    return <div>Chat</div>;
-}
--- src/app/lab/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <div className="max-w-screen-sm h-screen mx-auto relative">{children}</div>
-  );
-}
