4a09984f59b1daee71cdf1e634dc16616e6ce9ce
Savinay Kumar <savinay@zeliot.in>
2024-08-12 20:16:13 +0530

    added menu for mobile devices

--- src/components/navigation-bar/NavigationBar.tsx
+++ src/components/navigation-bar/NavigationBar.tsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { MenuIcon } from "lucide-react";
 export interface INavigationBarProps {}
 
 export default function NavigationBar(props: INavigationBarProps) {
@@ -9,6 +18,25 @@ export default function NavigationBar(props: INavigationBarProps) {
       <div className="w-full max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-14 items-center">
           <Link href="/" className="flex items-center" prefetch={false}>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <MenuIcon className="mr-2 md:hidden" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Avarta</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link href="/web" className="hover:underline py-4 px-3">
+                    Planet Avarta
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/web/team" className="hover:underline py-4 px-3">
+                    The Avartians
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
             <Image
               className=""
               src="/assets/common/avarta.png
